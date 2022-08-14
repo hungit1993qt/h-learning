dabcd3cfcb7acd9d01b9f1b946e19cb148f00c8c
hungit1993qt <hungit1993qt@gmail.com>
2022-08-14 19:59:44 +0700

    fix

--- src/Pages/Detail/ListCours.tsx
+++ src/Pages/Detail/ListCours.tsx
@@ -14,67 +14,60 @@ const ListCours = () => {
   const { khoaHocTheoDanhMuc, isLoading, error } = useSelector(
     (state: RootState) => state.khoaHocTheoDanhMuc
   );
+  console.log(khoaHocTheoDanhMuc);
   return (
     <section className={styles["detailListCourses"]}>
       <h1 className={styles["heading"]}>
         KHÓA <span> HỌC</span>
       </h1>
-      <div className={styles["grid-content"]}>
-        {khoaHocTheoDanhMuc.map((khoahoctheodanhmuc, index) => {
-          if (index<6) {
-            return (
-              <div
-                className={
-                  (index + 1) % 2 === 0
-                    ? `${styles["blog-card"]} ${styles["alt"]}`
-                    : styles["blog-card"]
-                }
-              >
-                <div className={styles["meta"]}>
-                  <div
-                    className={styles["photo"]}
-                    style={{
-                      backgroundImage: `url(${khoahoctheodanhmuc.hinhAnh})`,
-                    }}
-                  />
-                  <ul className={styles["details"]}>
-                    <li className={styles["author"]}>
-                      <a href="#">{khoahoctheodanhmuc.nguoiTao.hoTen}</a>
-                    </li>
-                    <li className={styles["date"]}>
-                      {khoahoctheodanhmuc.ngayTao}
-                    </li>
-                    <li className={styles["tags"]}>
-                      <ul>
-                        <li>
-                          <a href="#">Learn</a>
-                        </li>
-                        <li>
-                          <a href="#">Code</a>
-                        </li>
-                        <li>
-                          <a href="#">HTML</a>
-                        </li>
-                        <li>
-                          <a href="#">CSS</a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-                <div className={styles["description"]}>
-                  <h1>{khoahoctheodanhmuc.tenKhoaHoc}</h1>
-                  <h2>{khoahoctheodanhmuc.danhMucKhoaHoc.tenDanhMucKhoaHoc}</h2>
-                  <p>{khoahoctheodanhmuc.moTa}</p>
-                  <p className={styles["read-more"]}>
-                    <a href="#">Read More</a>
-                  </p>
-                </div>
+      
+        {khoaHocTheoDanhMuc.map((khoahoctheodanhmuc) => {
+          return (
+            <div className={styles["blog-card"]}>
+              <div className={styles["meta"]}>
+                <div
+                  className={styles["photo"]}
+                  style={{
+                    backgroundImage: `url(${khoahoctheodanhmuc.hinhAnh})`,
+                  }}
+                />
+                <ul className={styles["details"]}>
+                  <li className={styles["author"]}>
+                    <a href="#">{khoahoctheodanhmuc.nguoiTao.hoTen}</a>
+                  </li>
+                  <li className={styles["date"]}>
+                    {khoahoctheodanhmuc.ngayTao}
+                  </li>
+                  <li className={styles["tags"]}>
+                    <ul>
+                      <li>
+                        <a href="#">Learn</a>
+                      </li>
+                      <li>
+                        <a href="#">Code</a>
+                      </li>
+                      <li>
+                        <a href="#">HTML</a>
+                      </li>
+                      <li>
+                        <a href="#">CSS</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </div>
-            );
-          }
+              <div className={styles["description"]}>
+                <h1>{khoahoctheodanhmuc.tenKhoaHoc}</h1>
+                <h2>{khoahoctheodanhmuc.danhMucKhoaHoc.tenDanhMucKhoaHoc}</h2>
+                <p>{khoahoctheodanhmuc.moTa}</p>
+                <p className={styles["read-more"]}>
+                  <a href="#">Read More</a>
+                </p>
+              </div>
+            </div>
+          );
         })}
-      </div>
+      
     </section>
   );
 };
